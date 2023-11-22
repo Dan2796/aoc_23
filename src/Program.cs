9b45fa23a32ae3e882d59ff75eacc1b6744d9f5b
Dan Snow <danjsnow@outlook.com>
2023-11-22 17:04:23 +0000

    Initial commit with day 1 2022

---
+++ src/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Security.AccessControl;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        List<int> calorieNumbers = new List<int>();
+
+        int calorieCounter = 0;
+        using (StreamReader reader = new StreamReader("inputs/example/day_1_test.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                string calorie = reader.ReadLine();
+                if (!ObjectSecurity.Equals(calorie, "")) 
+                {
+                    calorieCounter += Int32.Parse(calorie);
+                } else 
+                {
+                    calorieNumbers.Add(calorieCounter);
+                    calorieCounter = 0;
+                }
+            }
+            }
+        // add last number in list:
+        calorieNumbers.Add(calorieCounter);
+        calorieNumbers.Sort();
+        calorieNumbers.Reverse();
+        Console.WriteLine(calorieNumbers[0]);  // Output the content
+        int total = calorieNumbers[0] + calorieNumbers[1] + calorieNumbers[2];
+        Console.WriteLine(total);  // Output the content
+        }
+    }
+}
