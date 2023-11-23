ce7a75e55c89cbde8facde48d4fa6935cbaf0ed3
Dan Snow <danjsnow@outlook.com>
2023-11-23 13:11:55 +0000

    Creates day function

--- src/Program.cs
+++ src/Program.cs
@@ -9,7 +9,60 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-        List<int> calorieNumbers = new List<int>();
+            Day.PrintSolutions(new Day02(false));
+        }
+    }
+}
+
+public abstract class Day
+{
+    public Day(Boolean actual)
+    {
+        String file;
+        if (actual) {
+            if (this.GetDay() < 10)
+            {
+                file = "inputs/actual/day_0" + this.GetDay() + ".txt";
+            } else
+            {
+                file = "inputs/actual/day_" + this.GetDay() + ".txt";
+            }
+        } else 
+        {
+            if (this.GetDay() < 10)
+            {
+                file = "inputs/example/day_0" + this.GetDay() + ".txt";
+            } else
+            {
+                file = "inputs/example/day_" + this.GetDay() + ".txt";
+            }
+        }
+    }
+
+    protected Day()
+    {
+    }
+
+    public abstract void ParseInput();
+    public abstract int GetDay();
+    public abstract Object GetSolutionPart1();
+    public abstract Object GetSolutionPart2();
+
+    public static void PrintSolutions(Day day) {
+        day.ParseInput();
+        Console.WriteLine("Day " + day.GetDay() + " solution to part 1: " + day.GetSolutionPart1().ToString());
+        Console.WriteLine("Day " + day.GetDay() + " solution to part 2: " + day.GetSolutionPart2().ToString());
+    }
+}
+
+class Day02(Boolean actual) : Day(actual)
+{
+    public override int GetDay() {
+        return 2;
+    }
+    List<int> calorieNumbers = new List<int>();
+
+    public override void ParseInput() {
 
         int calorieCounter = 0;
         using (StreamReader reader = new StreamReader("inputs/example/day_1_test.txt"))
@@ -31,9 +84,14 @@ namespace Day1
         calorieNumbers.Add(calorieCounter);
         calorieNumbers.Sort();
         calorieNumbers.Reverse();
-        Console.WriteLine(calorieNumbers[0]);  // Output the content
+    }
+
+    public override String GetSolutionPart1() {
+        return calorieNumbers[0].ToString();
+    }
+
+    public override String GetSolutionPart2() {
         int total = calorieNumbers[0] + calorieNumbers[1] + calorieNumbers[2];
-        Console.WriteLine(total);  // Output the content
-        }
+        return total.ToString();
     }
 }
