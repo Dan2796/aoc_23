08f2097b9836445aea835aea53c3acd4705778d7
Dan Snow <danjsnow@outlook.com>
2023-12-03 22:06:48 +0000

    Adds solution to day 1 part 1

--- src/days/day_01.cs
+++ src/days/day_01.cs
@@ -5,38 +5,41 @@ class Day01(Boolean actual) : Day(actual)
     public override int GetDay() {
         return 1;
     }
-    List<int> calorieNumbers = new List<int>();
+    List<string> input = new List<string>();
+    List<List<int>> inputDigits = new List<List<int>>();
 
     public override void ParseInput() {
 
-        int calorieCounter = 0;
         using (StreamReader reader = new StreamReader(GetFileName()))
-        {
+        { 
             while (!reader.EndOfStream)
             {
-                string calorie = reader.ReadLine();
-                if (!ObjectSecurity.Equals(calorie, "")) 
+              string inputLine = reader.ReadLine();
+              input.Add(inputLine);
+              List<int> justDigits = new List<int>();
+              foreach (char c in inputLine)
+              {
+                if (Char.IsDigit(c))
                 {
-                    calorieCounter += Int32.Parse(calorie);
-                } else 
-                {
-                    calorieNumbers.Add(calorieCounter);
-                    calorieCounter = 0;
+                  int charAsDigit = c - '0';
+                  justDigits.Add(charAsDigit);
                 }
+              }
+              inputDigits.Add(justDigits);
             }
-            }
-        // add last number in list:
-        calorieNumbers.Add(calorieCounter);
-        calorieNumbers.Sort();
-        calorieNumbers.Reverse();
+        }
     }
-
-    public override String GetSolutionPart1() {
-        return calorieNumbers[0].ToString();
+    
+    public override string GetSolutionPart1() {
+      int calibrationSum = 0;
+      foreach (List<int> digitLine in inputDigits) {
+        calibrationSum +=  digitLine[0] * 10;
+        calibrationSum +=  digitLine[digitLine.Count - 1];
+      }
+      return calibrationSum.ToString();
     }
 
     public override String GetSolutionPart2() {
-        int total = calorieNumbers[0] + calorieNumbers[1] + calorieNumbers[2];
-        return total.ToString();
+        return "tbd";
     }
 }
