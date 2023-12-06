44ce1d3dd5826cc29bf239692840b91d0faff1df
Dan Snow <danjsnow@outlook.com>
2023-12-06 19:45:29 +0000

    Refactors to use calibration tracker on Day1

--- src/days/Day01/Day01.cs
+++ src/days/Day01/Day01.cs
@@ -1,5 +1,3 @@
-using System.Globalization;
-using System.Security.AccessControl;
 using aoc_2023.src.days;
 
 class Day01(Boolean actual) : Day(actual)
@@ -24,115 +22,57 @@ class Day01(Boolean actual) : Day(actual)
             {
               continue;
             }
-            // Initialise with zero to avoid bug of assigning to array later
-            // after redefining with if statements
-            int first_digit_p1 = 0;
-            int first_digit_p2 = 0;
-            int second_digit_p1 = 0;
-            int second_digit_p2 = 0;
-            bool found_first_digit_p1= false;
-            bool found_first_digit_p2= false;
-            // check for digit 1 first by going forwards:
+            CalibrationTracker calibrationTracker = new();
             for (int i = 0; i < inputLine.Length; i++)
             {
-              if (Char.IsDigit(inputLine[i]))
-              {
-                if (!found_first_digit_p1)
-                {
-                  first_digit_p1 = inputLine[i] - '0';
-                  found_first_digit_p1 = true;
-                }
-                if (!found_first_digit_p2)
-                {
-                  first_digit_p2 = inputLine[i] - '0';
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p1 = inputLine[i] - '0';
-                second_digit_p2 = inputLine[i] - '0';
+              if (Char.IsDigit(inputLine[i])) {
+                calibrationTracker.UpdateDigits(inputLine[i] - '0');
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "zero", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "zero", inputLine)) 
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 0;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 0;
+                calibrationTracker.UpdatePart2Digits(0);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "one", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "one", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 1;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 1;
+                calibrationTracker.UpdatePart2Digits(1);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "two", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "two", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 2;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 2;
+                calibrationTracker.UpdatePart2Digits(2);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "three", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "three", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 3;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 3;
+                calibrationTracker.UpdatePart2Digits(3);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "four", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "four", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 4;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 4;
+                calibrationTracker.UpdatePart2Digits(4);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "five", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "five", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 5;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 5;
+                calibrationTracker.UpdatePart2Digits(5);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "six", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "six", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 6;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 6;
+                calibrationTracker.UpdatePart2Digits(6);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "seven", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "seven", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 7;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 7;
+                calibrationTracker.UpdatePart2Digits(7);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "eight", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "eight", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 8;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 8;
+                calibrationTracker.UpdatePart2Digits(8);
               }
-              if (Day01ExtraMethods.CheckStringMatch(i, "nine", inputLine))
+              if (CalibrationTracker.CheckStringMatch(i, "nine", inputLine))
               {
-                if (!found_first_digit_p2) {
-                  first_digit_p2 = 9;
-                  found_first_digit_p2 = true;
-                }
-                second_digit_p2 = 9;
+                calibrationTracker.UpdatePart2Digits(9);
               }
             }
-            calibrationNumbersP1.Add([first_digit_p1, second_digit_p1]);
-            calibrationNumbersP2.Add([first_digit_p2, second_digit_p2]);
+            calibrationNumbersP1.Add([calibrationTracker.GetFirstDigitP1(),
+                                      calibrationTracker.GetSecondDigitP1()]);
+            calibrationNumbersP2.Add([calibrationTracker.GetFirstDigitP2(),
+                                      calibrationTracker.GetSecondDigitP2()]);
         }
     }
     
