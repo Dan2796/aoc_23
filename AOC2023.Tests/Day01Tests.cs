3816866e645e0885284c3e230d25da592659dbed
Dan Snow <danjsnow@outlook.com>
2024-01-21 08:57:20 +0000

    Adds tests for Day 2

--- AOC2023.Tests/Day01Tests.cs
+++ AOC2023.Tests/Day01Tests.cs
@@ -22,21 +22,21 @@ public class Day01Tests
     }
     
     [Fact]
-    public void Day01_ParseAndSolveP2_ReturnCorrectAnswer()
-        {
-            // Arrange
-            Day01 day = new();
-            const string testInput = "two1nine\n" +
-                                     "eightwothree\n" +
-                                     "abcone2threexyz\n" +
-                                     "xtwone3four\n" +
-                                     "4nineeightseven2\n" +
-                                     "zoneight234\n" +
-                                     "7pqrstsixteen";
-            StreamReader testInputStream = MethodsForTesting.StringToStreamReader(testInput);
-            // Act
-            int result = day.ParseAndSolveP2(testInputStream);
-            // Assert
-            result.Should().Be(281);
-        }
+    public void Day01_ParseAndSolveP2_ReturnCorrectAnswer() 
+    {
+        // Arrange
+        Day01 day = new();
+        const string testInput = "two1nine\n" +
+                                 "eightwothree\n" +
+                                 "abcone2threexyz\n" +
+                                 "xtwone3four\n" +
+                                 "4nineeightseven2\n" +
+                                 "zoneight234\n" +
+                                 "7pqrstsixteen";
+        StreamReader testInputStream = MethodsForTesting.StringToStreamReader(testInput);
+        // Act
+        int result = day.ParseAndSolveP2(testInputStream);
+        // Assert
+        result.Should().Be(281);
+    }
 }
---
+++ AOC2023.Tests/Day02Tests.cs
@@ -0,0 +1,30 @@
+using AOC2023.Days.Day02;
+using FluentAssertions;
+
+namespace AOC2023.Tests;
+
+public class Day02Tests
+{
+    private const string TestInput = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n" +
+                                     "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n" + 
+                                     "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n" + 
+                                     "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n" + 
+                                     "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    private readonly Day02 _day = new();
+    private readonly StreamReader _testInputStream = MethodsForTesting.StringToStreamReader(TestInput);
+    
+    [Fact]
+    public void Day02_ParseAndSolveP1_ReturnCorrectAnswer()
+    { 
+        int result = _day.ParseAndSolveP1(_testInputStream);
+        result.Should().Be(8);
+    }
+    
+    [Fact]
+    public void Day02_ParseAndSolveP2_ReturnCorrectAnswer() 
+    { 
+        int result = _day.ParseAndSolveP2(_testInputStream);
+        result.Should().Be(2286);
+    }
+}
