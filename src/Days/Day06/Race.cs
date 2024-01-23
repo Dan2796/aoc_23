9cff6720862e7bd081376c9b4283d7a6e3182b1c
Dan Snow <danjsnow@outlook.com>
2024-01-23 09:09:34 +0000

    Implements function to get distance options

--- src/Days/Day06/Race.cs
+++ src/Days/Day06/Race.cs
@@ -1,5 +1,20 @@
-public class Race(int raceTime, int raceDistance)
+public class Race(int _raceTime, int _raceDistance)
 {
-    public int getTime => raceTime;
-    public int getDistance => raceDistance;
+    public int[] getDistanceOptions()
+    {
+        int[] options = new int[_raceTime];
+        for(int i = 0; i < _raceTime; i++)
+        {
+            options[i] = getRaceDistance(i, _raceTime);
+        }
+        return options;
+    }
+
+    public static int getRaceDistance(int buttonHoldTime, int raceTime)
+    {
+        int speed = buttonHoldTime;
+        int remainingTime = raceTime - buttonHoldTime;
+        return remainingTime * speed;
+    }
+        
 }
