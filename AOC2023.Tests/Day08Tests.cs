b8307977849a2512c7d9909c29b3dca96df5e7a5
Dan Snow <danjsnow@outlook.com>
2024-02-09 16:12:34 +0000

    Day 8 completed

--- AOC2023.Tests/Day08Tests.cs
+++ AOC2023.Tests/Day08Tests.cs
@@ -52,6 +52,59 @@ public class Day08Tests
     }
 
     [Fact]
+    public void CamelMaps_IsPrime_IdentifiesPrimes()
+    {
+        CamelMaps.IsPrime(100).Should().Be(false);
+        CamelMaps.IsPrime(244).Should().Be(false);
+        CamelMaps.IsPrime(2).Should().Be(true);
+        CamelMaps.IsPrime(9).Should().Be(false);
+        CamelMaps.IsPrime(32).Should().Be(false);
+    }
+   
+    [Fact] 
+    public void CamelMaps_CalcPrimeFactors_IdentifiesPrimeFactors()
+    {
+        Dictionary<long, int> fifteen = CamelMaps.CalcPrimeFactors(15);
+        fifteen[3].Should().Be(1);
+        fifteen[5].Should().Be(1);
+        fifteen.Keys.Should().NotContain(1);
+        Dictionary<long, int> oneHundred = CamelMaps.CalcPrimeFactors(100);
+        oneHundred[2].Should().Be(2);
+        oneHundred[5].Should().Be(2);
+        oneHundred.Keys.Should().NotContain(3);
+        Dictionary<long, int> two = CamelMaps.CalcPrimeFactors(2);
+        two[2].Should().Be(1);
+        Dictionary<long, int> three = CamelMaps.CalcPrimeFactors(3);
+        three[3].Should().Be(1);
+    }
+    
+    [Fact] 
+    public void CamelMaps_CalcLCM_GetsLCMOf23()
+    {
+        List<long> testMultiples = [ 2, 3 ];
+        long lcm = CamelMaps.CalcLCM(testMultiples);
+        lcm.Should().Be(6);
+    }
+    [Fact] 
+    public void CamelMaps_CalcLCM_GetsLCMOf61015()
+    {
+        List<long> testMultiples = [ 6, 10, 15 ];
+        long lcm = CamelMaps.CalcLCM(testMultiples);
+        lcm.Should().Be(30);
+    }
+   
+    [Fact]
+    public void CamelMaps_CartesianProduct_GetsRightNumberOfProducts()
+    {
+        IEnumerable<string> list1 = ["aa", "bb", "cc"];
+        IEnumerable<string> list2 = ["xx", "yy", "zz"];
+        IEnumerable<string> list3 = ["11", "22", "33"];
+        IEnumerable<IEnumerable<string>> test = [list1, list2, list3];
+        var result = CamelMaps.CartesianProduct(test);
+        result.Count().Should().Be(27);
+    }
+    
+    [Fact]
     public void Day08_ParseAndSolveP2_ReturnCorrectAnswer() 
     { 
         long result = _day.ParseAndSolveP2(_testInputStream3);
--- src/Days/Day08/CamelMaps.cs
+++ src/Days/Day08/CamelMaps.cs
@@ -21,22 +21,110 @@ public class CamelMaps(string lRSequence, Dictionary<string, (string, string)> m
 
     public long FollowAsIfGhost()
     {
-        List<string> currentPositions = [];
-        currentPositions.AddRange(mappings.Keys.Where(key => key[2] == 'A'));
-        int placeInLrSequence = 0;
-        long numberOfSteps = 0;
-        while (currentPositions.Any(position => position[2] != 'Z'))
-        {
-            // use for not foreach loop so replacement by index is easy
-            for (int i = 0; i < currentPositions.Count; i++)
-            {
-                currentPositions[i] = lRSequence[placeInLrSequence] == 'L'
-                    ? mappings[currentPositions[i]].Item1
-                    : mappings[currentPositions[i]].Item2;
-            }
-            numberOfSteps++;
-            placeInLrSequence = placeInLrSequence < lRSequence.Length - 1 ? placeInLrSequence + 1 : 0;
-        }
-        return numberOfSteps;
+        var startingPositions = mappings.Keys.Where(key => key[2] == 'A');
+        var multiples = startingPositions.Select(GetMultiples);
+        var multiplesCartesianProduct = CartesianProduct(multiples);
+        var allLCMs = multiplesCartesianProduct.Select(CalcLCM);
+        return allLCMs.Min();
     }
+
+    private List<long> GetMultiples(string position)
+    {
+        HashSet<string> visited = [];
+        int step = 0;
+        int placeInLrSequence = 0;
+        List<long> multiples = [];
+        string positionAndPlaceInLrSequence = position + placeInLrSequence;
+        while (!visited.Contains(positionAndPlaceInLrSequence))
+        {
+            visited.Add(positionAndPlaceInLrSequence);
+            position = lRSequence[placeInLrSequence] == 'L' 
+                ? mappings[position].Item1 
+                : mappings[position].Item2;
+            placeInLrSequence = placeInLrSequence < lRSequence.Length - 1 ? placeInLrSequence + 1 : 0;
+            positionAndPlaceInLrSequence = position + placeInLrSequence;
+            step++;
+            if (position[2] != 'Z') continue;
+            multiples.Add(step);
+        }
+        return multiples;
+    }
+
+   public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(IEnumerable<IEnumerable<T>> sequences) 
+   {
+     // base case: 
+     IEnumerable<IEnumerable<T>> result = 
+       new[] { Enumerable.Empty<T>() };
+     return sequences.Aggregate(result, (current, s) =>
+         from seq in current from item in s select seq.Concat(new[] { item })); 
+   } 
+
+    public static long CalcLCM(IEnumerable<long> multiples)
+    {
+        var primeFactors = multiples.Select(CalcPrimeFactors).ToList();
+        long lcm = 1; 
+        for (int i = 2; i <= multiples.Max(); i++)
+        {
+            int howMany = 0;
+            foreach (Dictionary<long, int> multiplePrimeFactors in primeFactors)
+            {
+                if (multiplePrimeFactors.Keys.Contains(i))
+                {
+                    howMany = Math.Max(multiplePrimeFactors[i], howMany);
+                }
+            }
+
+            if (howMany > 0)
+            {
+                lcm = howMany * i * lcm;
+            }
+        }
+        return lcm;
+    }
+
+    public static Dictionary<long, int> CalcPrimeFactors(long number)
+    {
+        Dictionary<long, int> primeFactors = new();
+        while (!IsPrime(number))
+        {
+            int i = 2;
+            while (number % i != 0 || !IsPrime(i))
+            {
+                i++;
+            }
+            
+            number /= i;
+            if (primeFactors.Keys.Contains(i))
+            {
+                primeFactors[i] += 1;
+            }
+            else
+            {
+                primeFactors.Add(i, 1);
+            }
+        }
+        if (primeFactors.Keys.Contains(number))
+        {
+            primeFactors[number] += 1;
+        }
+        else
+        {
+            primeFactors.Add(number, 1);
+        }
+        return primeFactors;
+    }
+    public static bool IsPrime(long number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        var boundary = (long)Math.Floor(Math.Sqrt(number));
+              
+        for (long i = 3; i <= boundary; i += 2)
+            if (number % i == 0)
+                return false;
+        return true;        
+    }
+        
 }
